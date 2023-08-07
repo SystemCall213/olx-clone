e417443180d0f17e9e47c840a3ed550bde4da55d
SystemCall213 <IzumiSensei@gmail.com>
2023-08-07 14:26:12 +0300

    Initial commit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Home from './+page.svelte'
+    import About from './about/+page.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<nav>
+    <a href="/" >Home</a>
+    <a href="/about" >About</a>
+</nav>
+
+<slot></slot>
