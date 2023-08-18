001b1e519b39549038cc6eb98e5af99f644e8259
SystemCall213 <IzumiSensei@gmail.com>
2023-08-18 18:50:01 +0300

    .

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 	},
 	preprocess: vitePreprocess()
 };
