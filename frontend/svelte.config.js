38149ce7d697b262059216b86122ac832a5f17eb
SystemCall213 <IzumiSensei@gmail.com>
2023-08-18 18:52:41 +0300

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,13 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 	},
 	preprocess: vitePreprocess()
 };
