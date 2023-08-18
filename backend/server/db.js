71fa3028514fa77992f6767a21bd89288b065273
SystemCall213 <IzumiSensei@gmail.com>
2023-08-18 22:47:13 +0300

    Update db.js

--- backend/server/db.js
+++ backend/server/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'postgres',
     password: 'gfhkfvtynd11',
-    host: 'localhost',
+    host: 'https://olx-clone-backend-two.vercel.app',
     port: 5432,
     database: 'olx_clone'
 })
